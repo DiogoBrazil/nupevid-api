9e8cc2bf5d202a06e47b8b7ab9f82fa89462e919
DiogoBrazil <diogoifroads@gmail.com>
2025-11-19 18:18:52 -0400

    feat: add authentication routes configuration for user login

--- src/routes/config/base_routes.rs
+++ src/routes/config/base_routes.rs
@@ -1,10 +1,10 @@
 use actix_web::web;
-use crate::routes::users;
+use crate::routes::{users, auth};
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v1")
             .configure(users::configure_routes)
-        //.configure(auth::configure_routes)
+            .configure(auth::configure_routes)
     );
 }
