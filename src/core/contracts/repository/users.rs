2b116521ca6bb4819f606962a27201c9450b4d98
DiogoBrazil <diogoifroads@gmail.com>
2025-11-21 06:17:51 -0400

    feat: enhance UserRepository with additional user management methods

--- src/core/contracts/repository/users.rs
+++ src/core/contracts/repository/users.rs
@@ -1,8 +1,9 @@
 use async_trait::async_trait;
+use uuid::Uuid;
 //use uuid::Uuid;
 use crate::core::entities::users::{
     CreateUser,
-    //UpdateUser,
+    UpdateUser,
     //UpdatePasswordUser,
     UserDataCreatedWithoutPassword,
     //CompleteUserData
@@ -12,4 +13,12 @@ use crate::core::entities::users::{
 #[async_trait]
 pub trait UserRepository: Send + Sync + 'static {
     async fn create_user(&self, data: CreateUser) -> Result<UserDataCreatedWithoutPassword, sqlx::Error>;
+    async fn get_user_by_id(&self, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error>;
+    async fn get_all_users(&self) -> Result<Vec<UserDataCreatedWithoutPassword>, sqlx::Error>;
+    async fn update_user_by_id(&self, data: UpdateUser, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error>;
+    async fn delete_user_by_id(&self, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error>;
+    async fn check_user_exists_by_email(&self, email: String) -> Result<bool, sqlx::Error>;
+    async fn check_email_exists_for_other_user(&self, email: &str, id: Uuid) -> Result<bool, sqlx::Error>;
+    async fn get_user_password_by_id(&self, id: Uuid) -> Result<String, sqlx::Error>;
+    async fn update_user_password_by_id(&self, id: Uuid, new_password: String) -> Result<UserDataCreatedWithoutPassword, sqlx::Error>;
 }
--- src/repositories/users.rs
+++ src/repositories/users.rs
@@ -9,6 +9,7 @@ use crate::core::{
     },
     entities::users::{
         CreateUser,
+        UpdateUser,
         UserDataCreatedWithoutPassword
     }
 };
@@ -47,4 +48,115 @@ impl UserRepository for PgUserRepository {
 
         Ok(user_created)
     }
+
+    async fn update_user_by_id(&self, data: UpdateUser, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error> {
+        info!("[Repository] Executing SQL query to update user with ID: {}", id);
+
+        let user_updated: UserDataCreatedWithoutPassword = sqlx::query_as(UsersQueries::UPDATE_USER_BY_ID)
+            .bind(id)
+            .bind(data.rank)
+            .bind(data.registration)
+            .bind(data.full_name)
+            .bind(data.profile)
+            .bind(data.email)
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] User successfully updated into database with ID: {}", id);
+
+        Ok(user_updated)
+    }
+
+    async fn get_user_by_id(&self, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error> {
+        info!("[Repository] Executing SQL query to get user with id: {}", id);
+
+        let user: UserDataCreatedWithoutPassword = sqlx::query_as(UsersQueries::GET_USER_BY_ID)
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] User successfully found in the database with IDD: {}", id);
+
+        Ok(user)
+
+    }
+
+    async fn check_user_exists_by_email(&self, email: String) -> Result<bool, sqlx::Error> {
+        info!("[Repository] Executing SQL query to check if user with email {} exists", email);
+
+        let user_exists: bool = sqlx::query_scalar(UsersQueries::CHECK_USER_EXISTS_BY_EMAIL)
+            .bind(email.clone())
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] User exists with email {}: {}", email, user_exists);
+
+        Ok(user_exists)
+    }
+
+    async fn check_email_exists_for_other_user(&self, email: &str, id: Uuid) -> Result<bool, sqlx::Error> {
+
+        info!("[Repository] Executing SQL query to check if email {} exists for other user", email);
+
+        let result: bool = sqlx::query_scalar(UsersQueries::CHECK_EMAIL_EXISTS_FOR_OTHER_USER)
+            .bind(email)
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        Ok(result)
+    }
+
+    async fn get_all_users(&self) -> Result<Vec<UserDataCreatedWithoutPassword>, sqlx::Error> {
+        info!("[Repository] Executing SQL query to get all users");
+
+        let users: Vec<UserDataCreatedWithoutPassword> = sqlx::query_as(UsersQueries::GET_ALL_USERS)
+            .fetch_all(&self.pool)
+            .await?;
+
+        info!("[Repository] Found {} users in database", users.len());
+
+        Ok(users)
+    }
+
+    async fn delete_user_by_id(&self, id: Uuid) -> Result<UserDataCreatedWithoutPassword, sqlx::Error> {
+        info!("[Repository] Executing SQL query to delete user with id: {}", id);
+
+        let deleted_user: UserDataCreatedWithoutPassword = sqlx::query_as(UsersQueries::DELETE_USER_BY_ID)
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] User successfully deleted from database with ID: {}", id);
+
+        Ok(deleted_user)
+    }
+
+    async fn get_user_password_by_id(&self, id: Uuid) -> Result<String, sqlx::Error> {
+        info!("[Repository] Executing SQL query to get password for user with id: {}", id);
+
+        let password: String = sqlx::query_scalar(UsersQueries::GET_USER_PASSWORD_BY_ID)
+            .bind(id)
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] Password retrieved successfully for user with ID: {}", id);
+
+        Ok(password)
+    }
+
+    async fn update_user_password_by_id(&self, id: Uuid, new_password: String) -> Result<UserDataCreatedWithoutPassword, sqlx::Error> {
+        info!("[Repository] Executing SQL query to update password for user with id: {}", id);
+
+        let updated_user: UserDataCreatedWithoutPassword = sqlx::query_as(UsersQueries::UPDATE_USER_PASSWORD_BY_ID)
+            .bind(id)
+            .bind(new_password)
+            .fetch_one(&self.pool)
+            .await?;
+
+        info!("[Repository] Password updated successfully for user with ID: {}", id);
+
+        Ok(updated_user)
+    }
+
 }
