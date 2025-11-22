41872ee4679f3b91b72520094d0dfcd1fd9ab1ab
DiogoBrazil <diogoifroads@gmail.com>
2025-11-22 16:24:18 -0400

    refactor: streamline attendance and victim repository traits; unify return types to include Address variants

--- src/core/contracts/repository/users.rs
+++ src/core/contracts/repository/users.rs
@@ -1,12 +1,9 @@
 use async_trait::async_trait;
 use uuid::Uuid;
-//use uuid::Uuid;
 use crate::core::entities::users::{
     CreateUser,
     UpdateUser,
-    //UpdatePasswordUser,
     UserDataCreatedWithoutPassword,
-    //CompleteUserData
 };
 
 
