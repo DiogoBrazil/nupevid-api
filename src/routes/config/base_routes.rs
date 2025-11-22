e5c505502162a51971ad01d820c5a777ad0eac94
DiogoBrazil <diogoifroads@gmail.com>
2025-11-21 20:35:40 -0400

    feat: update base routes to include attendances, cities, protective measures, and victims

--- src/routes/config/base_routes.rs
+++ src/routes/config/base_routes.rs
@@ -1,10 +1,14 @@
 use actix_web::web;
-use crate::routes::{users, auth};
+use crate::routes::{attendances, auth, cities, protective_measures, users, victims};
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api/v1")
-            .configure(users::configure_routes)
+            .configure(attendances::configure_routes)
             .configure(auth::configure_routes)
+            .configure(cities::configure_routes)
+            .configure(protective_measures::configure_routes)
+            .configure(users::configure_routes)
+            .configure(victims::configure_routes)
     );
 }
