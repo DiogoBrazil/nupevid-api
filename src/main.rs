b91e79b3718d062e7a450a7a15ce9aa26deb4973
DiogoBrazil <diogoifroads@gmail.com>
2025-12-04 06:35:06 -0400

    feat: integrate offender repository and service into main application

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use nupevid_api::repositories::{
     attendances::PgAttendanceRepository,
     auth::PgAuthRepository,
     cities::PgCityRepository,
+    offenders::PgOffenderRepository,
     protective_measures::PgProtectiveMeasureRepository,
     users::PgUserRepository,
     victims::PgVictimRepository,
@@ -16,7 +17,8 @@ use nupevid_api::repositories::{
 use nupevid_api::routes::config::base_routes::configure_routes;
 use nupevid_api::services::{
     attendances::AttendanceService, auth::AuthService, cities::CityService,
-    protective_measures::ProtectiveMeasureService, users::UserService, victims::VictimService,
+    offenders::OffenderService, protective_measures::ProtectiveMeasureService,
+    users::UserService, victims::VictimService,
 };
 
 #[actix_web::main]
@@ -45,6 +47,7 @@ async fn main() -> std::io::Result<()> {
     let auth_repository = web::Data::new(PgAuthRepository::new(pool.clone()));
     let city_repository = web::Data::new(PgCityRepository::new(pool.clone()));
     let victim_repository = web::Data::new(PgVictimRepository::new(pool.clone()));
+    let offender_repository = web::Data::new(PgOffenderRepository::new(pool.clone()));
     let protective_measure_repository = web::Data::new(PgProtectiveMeasureRepository::new(pool.clone()));
     let attendance_repository = web::Data::new(PgAttendanceRepository::new(pool.clone()));
     info!("Repositories created");
@@ -62,6 +65,7 @@ async fn main() -> std::io::Result<()> {
     ));
     let city_service = web::Data::new(CityService::new(city_repository.clone(), user_repository.clone()));
     let victim_service = web::Data::new(VictimService::new(victim_repository.clone(), user_repository.clone()));
+    let offender_service = web::Data::new(OffenderService::new(offender_repository.clone(), user_repository.clone()));
     let protective_measure_service = web::Data::new(ProtectiveMeasureService::new(
         protective_measure_repository.clone(),
         victim_repository.clone(),
@@ -93,12 +97,14 @@ async fn main() -> std::io::Result<()> {
             .app_data(auth_repository.clone())
             .app_data(city_repository.clone())
             .app_data(victim_repository.clone())
+            .app_data(offender_repository.clone())
             .app_data(protective_measure_repository.clone())
             .app_data(attendance_repository.clone())
             .app_data(user_service.clone())
             .app_data(auth_service.clone())
             .app_data(city_service.clone())
             .app_data(victim_service.clone())
+            .app_data(offender_service.clone())
             .app_data(protective_measure_service.clone())
             .app_data(attendance_service.clone())
             .app_data(web::Data::new(config.clone()))
