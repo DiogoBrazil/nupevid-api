fd787c78208753638d39ab5e1c340e6b539edfb8
DiogoBrazil <diogoifroads@gmail.com>
2025-12-07 15:44:02 -0400

    fix: change email parameter type to &str for get_complete_user_data_by_email method

--- src/core/contracts/repository/auth.rs
+++ src/core/contracts/repository/auth.rs
@@ -3,5 +3,5 @@ use crate::core::entities::auth::CompleteUserData;
 
 #[async_trait]
 pub trait AuthRepository {
-    async fn get_complete_user_data_by_email(&self, email: String) -> Result<CompleteUserData, sqlx::Error>;
+    async fn get_complete_user_data_by_email(&self, email: &str) -> Result<CompleteUserData, sqlx::Error>;
 }
