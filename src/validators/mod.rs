768185324cafdff59032be066195d3d86c4092b4
DiogoBrazil <diogoifroads@gmail.com>
2025-12-08 20:37:01 -0400

    fix: rename attendance_validator to attendance_victim_validator for consistency

--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -4,5 +4,5 @@ pub mod policy_validator;
 pub mod city_validator;
 pub mod victim_validator;
 pub mod offender_validator;
-pub mod attendance_validator;
+pub mod attendance_victim_validator;
 pub mod protective_measure_validator;
