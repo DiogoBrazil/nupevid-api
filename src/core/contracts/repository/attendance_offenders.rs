33f6678ae9d21b1a77c0b943b9b580428fb41059
DiogoBrazil <diogoifroads@gmail.com>
2025-12-09 17:22:21 -0400

    feat: add AttendanceOffenderRepository trait with CRUD operations

---
+++ src/core/contracts/repository/attendance_offenders.rs
@@ -0,0 +1,15 @@
+use async_trait::async_trait;
+use uuid::Uuid;
+
+use crate::core::entities::attendance_offenders::{AttendanceOffenderWithAddress, CreateAttendanceOffender, UpdateAttendanceOffender};
+
+#[async_trait]
+pub trait AttendanceOffenderRepository: Send + Sync {
+    async fn create_attendance_offender(&self, attendance: CreateAttendanceOffender,) -> Result<AttendanceOffenderWithAddress, sqlx::Error>;
+    async fn get_attendance_offender_by_id(&self, id: Uuid) -> Result<AttendanceOffenderWithAddress, sqlx::Error>;
+    async fn get_all_attendance_offenders(&self) -> Result<Vec<AttendanceOffenderWithAddress>, sqlx::Error>;
+    async fn get_attendance_offenders_by_offender(&self, offender_id: Uuid,) -> Result<Vec<AttendanceOffenderWithAddress>, sqlx::Error>;
+    async fn get_attendance_offenders_by_victim(&self, victim_id: Uuid,) -> Result<Vec<AttendanceOffenderWithAddress>, sqlx::Error>;
+    async fn update_attendance_offender_by_id(&self, data: UpdateAttendanceOffender, id: Uuid,) -> Result<AttendanceOffenderWithAddress, sqlx::Error>;
+    async fn delete_attendance_offender_by_id(&self, id: Uuid) -> Result<AttendanceOffenderWithAddress, sqlx::Error>;
+}
