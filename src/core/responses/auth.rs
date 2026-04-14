0846df38432051eddd4e9205b4793b7a3c11dcf1
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-14 16:33:44 -0400

    refactor: update LoginResponse struct to use structured types for rank and profile

--- src/core/responses/auth.rs
+++ src/core/responses/auth.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::core::read_models::work_sessions::WorkSessionWithMembers;
+use crate::core::value_objects::profiles::Profile;
+use crate::core::value_objects::ranks::Rank;
 
 #[derive(Serialize, Deserialize)]
 pub struct LoginResponse {
@@ -9,8 +11,8 @@ pub struct LoginResponse {
     pub id: Uuid,
     pub full_name: String,
     pub email: String,
-    pub rank: String,
+    pub rank: Rank,
     pub registration: String,
-    pub profile: String,
+    pub profile: Profile,
     pub work_session: Option<WorkSessionWithMembers>,
 }
