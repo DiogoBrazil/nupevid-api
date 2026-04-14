205853b819816cc52005be4bf7fbe2b059da2a53
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-14 16:33:39 -0400

    refactor: update UserComplement struct to use structured types for rank and profile

--- src/core/read_models/users.rs
+++ src/core/read_models/users.rs
@@ -2,13 +2,16 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 use uuid::Uuid;
 
+use crate::core::value_objects::profiles::Profile;
+use crate::core::value_objects::ranks::Rank;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UserComplement {
     pub id: Uuid,
-    pub rank: String,
+    pub rank: Rank,
     pub registration: String,
     pub full_name: String,
-    pub profile: String,
+    pub profile: Profile,
     pub email: String,
     pub city_id: Option<Uuid>,
     pub permission_policies: JsonValue,
