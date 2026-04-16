0322034f0e78aeb151a8f609201ec47a8f91a266
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-16 18:17:18 -0400

    refactor: reorganize module declarations in lib.rs for improved structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod adapters;
+pub mod app_factory;
 pub mod config;
 pub mod controllers;
 pub mod core;
 pub mod middleware;
+pub mod presenters;
 pub mod repositories;
 pub mod routes;
-pub mod services;
+pub mod usecases;
 pub mod utils;
 pub mod validators;
