dacb1c4f78652d5a5a1b6df4eda61199e1c53372
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-17 18:06:58 -0400

    refactor: remove unused default_for_profile function and related imports

--- src/core/value_objects/policies.rs
+++ src/core/value_objects/policies.rs
@@ -1,9 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::fmt;
-use uuid::Uuid;
-
-use crate::core::value_objects::profiles::Profile;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Policy {
@@ -156,65 +152,6 @@ impl Policy {
             Policy::ViewOtherWorkSessions,
             Policy::ManageAttendanceMembers,
         ]
-    }
-
-    pub fn default_for_profile(
-        profile: &Profile,
-        city_id: Option<Uuid>,
-    ) -> HashMap<Policy, Vec<Uuid>> {
-        let mut policies: HashMap<Policy, Vec<Uuid>> = HashMap::new();
-
-        let policy_list: &[Policy] = match profile {
-            Profile::Root => return policies,
-            Profile::CityAdmin => &[
-                Policy::ReadCities,
-                Policy::CreateUsers,
-                Policy::ReadUsers,
-                Policy::UpdateUsers,
-                Policy::DeleteUsers,
-                Policy::CreateVictims,
-                Policy::ReadVictims,
-                Policy::UpdateVictims,
-                Policy::DeleteVictims,
-                Policy::CreateOffenders,
-                Policy::ReadOffenders,
-                Policy::UpdateOffenders,
-                Policy::DeleteOffenders,
-                Policy::CreateAttendances,
-                Policy::ReadAttendances,
-                Policy::UpdateAttendances,
-                Policy::DeleteAttendances,
-                Policy::CreateProtectiveMeasures,
-                Policy::ReadProtectiveMeasures,
-                Policy::UpdateProtectiveMeasures,
-                Policy::DeleteProtectiveMeasures,
-                Policy::CreateWorkSessions,
-                Policy::UpdateWorkSessions,
-                Policy::EndWorkSessions,
-                Policy::ViewOtherWorkSessions,
-                Policy::ManageAttendanceMembers,
-            ],
-            Profile::CityUser => &[
-                Policy::ReadCities,
-                Policy::ReadVictims,
-                Policy::ReadOffenders,
-                Policy::ReadAttendances,
-                Policy::ReadProtectiveMeasures,
-                Policy::ReadUsers,
-                Policy::CreateWorkSessions,
-                Policy::UpdateWorkSessions,
-                Policy::EndWorkSessions,
-                Policy::ManageAttendanceMembers,
-            ],
-        };
-
-        if let Some(cid) = city_id {
-            for policy in policy_list {
-                policies.insert(policy.clone(), vec![cid]);
-            }
-        }
-
-        policies
     }
 }
 
