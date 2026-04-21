130f3c1402f2347bee79306b11fe825d5f536ffe
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-21 15:59:46 -0400

    refactor: remove ProtectiveMeasureExtension from ProtectiveMeasure structs

--- src/core/read_models/protective_measures.rs
+++ src/core/read_models/protective_measures.rs
@@ -1,22 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-use crate::core::entities::protective_measures::{ProtectiveMeasure, ProtectiveMeasureExtension};
+use crate::core::entities::protective_measures::ProtectiveMeasure;
 use crate::core::read_models::cities::CitySummary;
 use crate::core::read_models::offenders::OffenderSummary;
 use crate::core::read_models::victims::VictimSummary;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ProtectiveMeasureWithExtensions {
+pub struct ProtectiveMeasureSimple {
     #[serde(flatten)]
     pub measure: ProtectiveMeasure,
-    pub extensions: Vec<ProtectiveMeasureExtension>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ProtectiveMeasureWithRelations {
     #[serde(flatten)]
     pub measure: ProtectiveMeasure,
-    pub extensions: Vec<ProtectiveMeasureExtension>,
     pub victim: VictimSummary,
     pub offender: OffenderSummary,
     pub court_district: CitySummary,
