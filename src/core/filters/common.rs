8066042cc717f3ae32ce1adc8fe13f19fd69ed90
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-22 19:44:25 -0400

    feat: add alias for include_related_entities and update E2E test helpers formatting

--- src/core/filters/common.rs
+++ src/core/filters/common.rs
@@ -2,5 +2,6 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct IncludeRelatedQuery {
+    #[serde(alias = "include_complement_for_entities")]
     pub include_related_entities: Option<bool>,
 }
