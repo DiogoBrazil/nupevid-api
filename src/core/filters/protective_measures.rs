a973ef9aae52889f2284fe7c4cfc80bfda9fe13b
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-22 19:44:36 -0400

    feat: update API key, reformat E2E helpers, and add serde aliases for related entities filters

--- src/core/filters/protective_measures.rs
+++ src/core/filters/protective_measures.rs
@@ -4,5 +4,6 @@ use serde::Deserialize;
 pub struct ProtectiveMeasuresQuery {
     pub page: Option<i64>,
     pub page_size: Option<i64>,
+    #[serde(alias = "include_complement_for_entities")]
     pub include_related_entities: Option<bool>,
 }
