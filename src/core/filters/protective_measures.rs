7c5bd1e6b6caf6afbfd3afdbbc096203f25be4b2
Diogo Ribeiro <diogoifroads@gmail.com>
2026-06-05 18:02:05 -0400

    feat: add victim_id and offender_id fields to ProtectiveMeasuresQuery struct

--- src/core/filters/protective_measures.rs
+++ src/core/filters/protective_measures.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct ProtectiveMeasuresQuery {
@@ -6,4 +7,6 @@ pub struct ProtectiveMeasuresQuery {
     pub page_size: Option<i64>,
     #[serde(alias = "include_complement_for_entities")]
     pub include_related_entities: Option<bool>,
+    pub victim_id: Option<Uuid>,
+    pub offender_id: Option<Uuid>,
 }
