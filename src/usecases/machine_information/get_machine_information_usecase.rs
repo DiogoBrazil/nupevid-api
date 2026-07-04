959bb6ea3c6d2e4207be0f6f8672202af8637f0c
Diogo Ribeiro <diogoifroads@gmail.com>
2026-07-04 18:34:16 -0400

    feat: add authorization check for accessing machine information

--- src/usecases/machine_information/get_machine_information_usecase.rs
+++ src/usecases/machine_information/get_machine_information_usecase.rs
@@ -1,5 +1,7 @@
 use crate::core::application_error::ApplicationError as AppError;
 use crate::core::contracts::adapters::system_metrics::MachineInformation;
+use crate::core::entities::auth::UserClaims;
+use crate::core::value_objects::profiles::Profile;
 
 use super::deps::MachineInformationUseCaseDependencies;
 
@@ -12,7 +14,13 @@ impl GetMachineInformationUseCase {
         Self { deps }
     }
 
-    pub async fn execute(&self) -> Result<MachineInformation, AppError> {
+    pub async fn execute(&self, claims: &UserClaims) -> Result<MachineInformation, AppError> {
+        if claims.profile != Profile::Root {
+            return Err(AppError::Forbidden(
+                "Only ROOT can access machine information".to_string(),
+            ));
+        }
+
         Ok(self.deps.system_metrics.collect().await)
     }
 }
